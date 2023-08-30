ac50793c3bbfe1db05107737dae4d9e429c8ba38
Ashraf-2 <hasanashraful03@gmail.com>
2023-08-30 22:13:00 +0600

    color change

--- scripts/phone.js
+++ scripts/phone.js
@@ -7,7 +7,7 @@ const loadPhone = async (SearchText, isShowAll) => {
     // console.log(phones);
     displayPhones(phones,isShowAll);
 }
-const displayPhones = (phones_p,isShowAll) => {
+const displayPhones = (phones_data,isShowAll) => {
     // console.log(parameter);
     const phoneContainerElement = document.getElementById('phone-container');
     
@@ -17,9 +17,9 @@ const displayPhones = (phones_p,isShowAll) => {
     // console.log(`number of ${SearchText_p}: ${phones_p.length}`);
     //create h2 for showing the number of phones
 
-    //display show all button if there sare more than 12 phones
-    const showAllDiv = document.getElementById('show-all-div');
-    if(phones_p.length >= 12 && !isShowAll){
+    //display show all button if there are more than 12 phones
+    const showAllDiv = document.getElementById('show-all-button-div');
+    if(phones_data.length >= 12 && !isShowAll){
         showAllDiv.classList.remove('hidden');
     }
     else{
@@ -28,13 +28,13 @@ const displayPhones = (phones_p,isShowAll) => {
     
     //display only first 12 phones if not 12phones
     if(!isShowAll){
-        phones_p = phones_p.slice(0,12);                 //to show the number of arrays of phones.not all the phones 
+        phones_data = phones_data.slice(0,12);                 //to show the number of arrays of phones.not all the phones 
     }
-    phones_p.forEach(phone => {
+    phones_data.forEach(phone => {
         console.log(phone)
-        // s-2: create div
+        // s-2: create div-creating div for phones-card
         const phoneCard = document.createElement('div');
-        phoneCard.classList = 'card w-96 bg-gray-200 shadow-xl py-5';
+        phoneCard.classList = 'card w-96 bg-sky-100 shadow-xl py-5';
         //s-3: set inner html
         phoneCard.innerHTML = `
         <figure>
@@ -66,8 +66,6 @@ const handleShowDetail = async (id)=>{
     console.log(data);
 
     showPhoneDetetails(phone);
-
-
 }
 
 const showPhoneDetetails = (phone_p) =>{
@@ -81,17 +79,10 @@ const showPhoneDetetails = (phone_p) =>{
     <p><span>Storage: </span>${phone_p.mainFeatures.storage}</p>
     <p><span>Display Size: </span>${phone_p.mainFeatures.displaySize}</p>
     <p><span>GPS: </span>${phone_p.others?.GPS? phone_p.others.GPS: 'No GPS Available in this Device'}</p>
-    
 
     `
-
     show_details_modal.showModal()
-
-
 }
-
-
-
 
 //handle search button
 const handleSearch = (isShowAll) =>{
@@ -99,7 +90,9 @@ const handleSearch = (isShowAll) =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
+    console.log('isShowALL: ',isShowAll);
     loadPhone(searchText, isShowAll);
+
     // searchText='';
 }
 //handle search recap (2)
@@ -108,7 +101,6 @@ const handleSearch2 = () =>{
     const searchField2 = document.getElementById('search-field2');
     const searchText2 = searchField2.value;
     loadPhone(searchText2);
-
 }
 const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinnerElement = document.getElementById('loading-spinner');
@@ -123,7 +115,6 @@ const toggleLoadingSpinner = (isLoading) => {
 //handle show all
 const handleShowAll = () =>{
     handleSearch(true);
-
 }
 
 // loadPhone()
